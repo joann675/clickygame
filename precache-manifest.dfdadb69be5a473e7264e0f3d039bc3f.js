dceaf7020e8354417a1e59bb7cdf082969d91cea
joann675 <joann675@yahoo.com>
2019-04-04 19:37:34 -0400

    Updates

---
+++ precache-manifest.dfdadb69be5a473e7264e0f3d039bc3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "f95001fdede020540e89",
+    "url": "/clickygame/static/js/main.1ee09ec5.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "f95001fdede020540e89",
+    "url": "/clickygame/static/css/main.71b1c5aa.chunk.css"
+  },
+  {
+    "revision": "f3a4eef2ad014d2eb1e62f7661d9dbf2",
+    "url": "/clickygame/index.html"
+  }
+];
